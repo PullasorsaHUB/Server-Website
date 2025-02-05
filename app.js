e9f75b2484c800acc07922d513960b3434eefbf4
PullasorsaHUB <rasmus.puromaki@hotmail.fi>
2025-02-05 18:33:09 +0200

    Lisättiin dropdown button ja javascriptiin animaatio

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const menu = document.querySelector('#mobile-menu')
-const menulinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menulinks = document.querySelector('.navbar__menu');
 
 menu.addEventListener('click', function()
 {
