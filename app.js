ab41a19d6d0be5141630550e26f5a324045f9f3d
Rasmus Puromäki <rasmus.puromaki@hotmail.fi>
2025-02-07 22:27:49 +0200

    dropdown done

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 const menu = document.querySelector('#mobile-menu');
 const menulinks = document.querySelector('.navbar__menu');
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li'); // FIXED
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => { // FIXED
+        select.classList.toggle('select-clicked'); // FIXED
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked'); // FIXED
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+
+            options.forEach(opt => { // Ensure we remove 'active' from all options
+                opt.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
+
+
+
 
 menu.addEventListener('click', function()
 {
